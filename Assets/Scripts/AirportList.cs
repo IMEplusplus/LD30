ee879f341d28f42412adac861c76f7778266e432
Naum Azeredo <naumazeredo@gmail.com>
2014-08-24 20:08:32 -0300

    Improved AirportList

--- Assets/Scripts/AirportList.cs
+++ Assets/Scripts/AirportList.cs
@@ -4,10 +4,33 @@ using System.Collections.Generic;
 
 public class AirportList : MonoBehaviour
 {
+    public int initialAirportsQuantity = 5;
+
     public Airport[] airports;
+    public List<Airport> available;
+    public List<Airport> hidden;
 
     void Start()
     {
         airports = GetComponentsInChildren<Airport>();
+        available.Clear();
+
+        foreach (Airport a in airports)
+        {
+            a.gameObject.SetActive(false);
+            hidden.Add(a);
+        }
+
+        for (int i = 0; i < initialAirportsQuantity; ++i)
+        {
+            int rand = Random.Range(0, hidden.Count);
+            hidden[rand].gameObject.SetActive(true);
+            available.Add(hidden[rand]);
+            hidden.RemoveAt(rand);
+        }
+    }
+
+    void Update()
+    {
     }
 }
