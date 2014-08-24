b7482f8fb3f626fea20ad4b5b4b9e9bc5b10db63
Roberto Rezende <rrezende88@gmail.com>
2014-08-24 17:24:48 -0300

    Model
    Plane, Route, Airport

---
+++ Assets/Scripts/Model/Airport.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Airport : MonoBehaviour {
+
+	public int Id { get; set; }
+	
+	public List<Plane> PlaneList { get; set; }
+	
+	public Dictionary<Airport,int> AirportPassengerCountDictionary { get; set; } 
+	
+	public Vector2 Position { get; set; }
+	
+	public readonly int Capacity;
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
