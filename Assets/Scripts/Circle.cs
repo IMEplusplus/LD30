9bf9a6e987df32965e0fef90c35002bd897ade10
Naum Azeredo <naumazeredo@gmail.com>
2014-08-25 01:04:09 -0300

    Created a minimun circle on airport and color change

--- Assets/Scripts/Circle.cs
+++ Assets/Scripts/Circle.cs
@@ -7,19 +7,33 @@ public class Circle : MonoBehaviour
     private Airport airport;
     public float size;
 
-	// Use this for initialization
+    SpriteRenderer sprite;
+
+    void OnEnable()
+    {
+        size = Constants.instance.circleMinSize;
+
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        sprite.color = Constants.instance.circleInitialColor;
+    }
+	
 	void Start ()
     {
         airport = GetComponentInParent<Airport>();
 	}
 	
-	// Update is called once per frame
 	void Update ()
     {
         if (airport.capacity != 0)
         {
-            size = (float)airport.passengers / airport.capacity;
+            var ratio = (float)airport.passengers / airport.capacity;
+            var min = Constants.instance.circleMinSize;
+            size = min + (1 - min) * ratio;
             transform.localScale = new Vector3(size, size, 1.0f);
+
+            //var grad = Constants.instance.circleInitialColor - Constants.instance.circleFinalColor;
+            var grad = Constants.instance.circleFinalColor - Constants.instance.circleInitialColor;
+            sprite.color = Constants.instance.circleInitialColor + ratio * grad;
         }
         else
         {
